8b0ef7713aaf75bf705ebd167674e238d9229a46
viktorpavlov1 <v.pavlovbul@gmail.com>
2025-07-30 00:03:10 +0200

    atabase connection set up

---
+++ bookBazaar/src/main/java/com/bookBazaar/BookBazaarApplication.java
@@ -0,0 +1,13 @@
+package com.bookBazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBazaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBazaarApplication.class, args);
+	}
+
+}
